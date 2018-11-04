f8d9279d256fe1ab3e6f7bf12eef0bc0c0620260
Adrian <macylapka1@gmail.com>
2018-11-04 22:52:30 +0100

    degrees to radians converted

--- mechanics.py
+++ mechanics.py
@@ -5,20 +5,17 @@ import utils
 import math
 
 from series   import maclaurin
-from series   import trigonometric
 from capacity import engine_capacity
 
 
 def pistone_stroke(R, L, alfa, beta):
-  return R + L - (R * math.cos(math.radians(alfa)) + \
-         L * math.cos(math.radians(beta)))
+  return R + L - (R * math.cos(math.radians(alfa)) + L * math.cos(math.radians(beta)))
 
 
 def pistone_stroke_2(R, L, alfa):
- return R * ((1 - math.cos(alfa)) + ((R / L) / 4 * (1 - math.cos(2 * alfa))))
+ return R * ((1 - math.cos(math.radians(alfa))) + ((R / L) / 4 * (1 - math.cos(math.radians(2 * alfa)))))
 
-def main():
-    
+def main(): 
   print("pistone_stroke = %f" % pistone_stroke(50, 25, 20, 10))
   print("pistone_stroke_2 = %f" % pistone_stroke_2(50, 25, 20)) 
 
